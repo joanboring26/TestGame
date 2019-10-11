19cac78b2c887476390be03d3f1f28d21ccdfc2e
Joan <joan.pato@enti.cat>
2019-10-11 09:51:30 +0200

    Camera system
    Done

---
+++ Assets/Scripts/Player/CameraMover.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CameraMover : MonoBehaviour
+{
+    //Recoil y cLerp determinan la posicion actual de la cámara entre el Mouse y el jugador, 0.5 = 50% de posicion, osea que está en medio de la posicion
+    /*
+    private float CAM_POS = 0.5f;
+    float recoil = 0.5f;
+    */
+    public Transform cameraShakeHolder;
+    public Transform player;
+
+
+    float cLerp = 0.5f;
+
+    float shakeAmt;
+
+    public float recoveryRate;
+    public float maxRecoil;
+    public float recoilDuration;
+
+
+    public float mouseRange;
+
+    private void FixedUpdate()
+    {
+        cLerp = Mathf.Lerp(cLerp, 0.5f, recoveryRate);
+        transform.position = Vector3.Lerp(MousePointer.MousePos, player.position, cLerp);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, player.position.x - mouseRange, player.position.x + mouseRange), transform.position.y, Mathf.Clamp(transform.position.z, player.position.z - mouseRange, player.position.z + mouseRange));
+
+    }
+
+    public void camShake(float recoil)
+    {
+        shakeAmt = recoil;
+        InvokeRepeating("DoShake", 0, 0.01f);
+        Invoke("StopShake", recoilDuration);
+
+    }
+
+    public void DoShake()
+    {
+        if(shakeAmt > 0)
+        {
+            Vector3 camPos = cameraShakeHolder.position;
+
+            float offsetX = Random.Range(-shakeAmt, shakeAmt) * - 1.2f;
+            float offsetY = Random.Range(-shakeAmt, shakeAmt) * - 1.2f;
+
+            camPos.x += offsetX;
+            camPos.z += offsetY;
+
+            cameraShakeHolder.transform.position = camPos;
+        }
+    }
+
+    public void StopShake()
+    {
+        CancelInvoke("DoShake");
+        cameraShakeHolder.localPosition = Vector3.zero;
+    }
+}
+
---
+++ Assets/Scripts/Player/MousePointer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    public float MouseSensitivity;
+    public float camHeight;
+    static public Vector3 MousePos;
+    float camHorizontal;
+    float camVertical;
+
+    private Camera cam;
+
+    private Vector3 worldCamPos;
+    private Vector2 ScreenCamPos;
+
+    private Event currentEvent;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        cam = Camera.main;
+    }
+
+    private void OnGUI()
+    {
+        currentEvent = Event.current;
+        ScreenCamPos.x = currentEvent.mousePosition.x;
+        ScreenCamPos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        worldCamPos = cam.ScreenToWorldPoint(new Vector3(ScreenCamPos.x, ScreenCamPos.y, cam.nearClipPlane));
+
+        //worldCamPos = new Vector3(Mathf.Clamp(worldCamPos.x, PlayerMov.playerPos.x - mouseRange, PlayerMov.playerPos.x + mouseRange), 1.87f, Mathf.Clamp(worldCamPos.z, PlayerMov.playerPos.z - mouseRange, PlayerMov.playerPos.z + mouseRange));
+        worldCamPos = new Vector3( worldCamPos.x, camHeight, worldCamPos.z);
+
+        MousePos = worldCamPos;
+        transform.position = MousePos;
+    }
+}
