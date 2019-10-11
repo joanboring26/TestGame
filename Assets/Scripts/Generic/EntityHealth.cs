89c9583cbdf1e6c32dc9ebe993a6fe60dcef5a40
Joan <joan.pato@enti.cat>
2019-10-11 10:46:09 +0200

    More work on player
    What it says

---
+++ Assets/Scripts/Generic/EntityHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityHealth : MonoBehaviour
+{
+    public float hp;
+
+    //Script will broadcast this message to the thing it is attached to when it dies
+    public string deathMessage;
+
+    // Start is called before the first frame update
+    
+
+    void ModHealth(float givVal)
+    {
+        hp += givVal;
+
+        if(hp <= 0)
+        {
+            BroadcastMessage(deathMessage, hp);
+        }
+    }
+}
