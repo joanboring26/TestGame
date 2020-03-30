b5e1fc82a9e7b0ec16ddaa19f626853ebcda608f
Cuesta <david.cuesta@enti.cat>
2020-03-30 19:16:52 +0200

    Level locking and bitshifting fully workable

--- Assets/LevelLocking.cs
+++ Assets/LevelLocking.cs
@@ -10,6 +10,7 @@ public class LevelLocking : MonoBehaviour
     int Levels; 
 
    public GameObject[] Level;
+    public GameObject[] Lock;
 
     private void Start()
     {
@@ -19,6 +20,7 @@ public class LevelLocking : MonoBehaviour
         for (int i = 0; i < Level.Length; i++)
         {
             Level[i].SetActive((Levels >> i) % 2 == 1);
+            Lock[i].SetActive((Levels >> i) % 2 == 0);
         }
     }
 
@@ -33,6 +35,7 @@ public class LevelLocking : MonoBehaviour
             for (int i = 0; i < Level.Length; i++)
             {
                 Level[i].SetActive((Levels >> i) % 2 == 1);
+                Lock[i].SetActive((Levels >> i) % 2 == 0);
             }
         }
 
@@ -43,6 +46,7 @@ public class LevelLocking : MonoBehaviour
             {
                 Levels += (int)Mathf.Pow(2f, i);
                 Level[i].SetActive((Levels >> i) % 2 == 1);
+                Lock[i].SetActive((Levels >> i) % 2 == 0);
             }
 
             PlayerPrefs.SetInt("Levels", Levels);
