1d80582e4f157022c2d36cfa1dad4367619d2f15
Joan Pato <joan.pato@enti.cat>
2020-04-02 19:13:38 +0200

    Level 5 fixed

---
+++ Assets/Scripts/SceneScripts/Doors/TriggerAddDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAddDoor : MonoBehaviour
+{
+    public GameObject[] door;
+    public float addAmount;
+    public bool disableOnTrigger;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < door.Length; i++)
+        {
+            door[i].SendMessage("eventTrig", addAmount);
+
+        }
+        if(disableOnTrigger)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
